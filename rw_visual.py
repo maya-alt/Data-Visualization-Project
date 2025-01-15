234fad3ea179e30921abf80720b9795828e41a40
maya-alt <mayafisher100@gmail.com>
2025-01-15 10:13:53 -0700

    Update rw_visual.py

--- rw_visual.py
+++ rw_visual.py
@@ -4,16 +4,22 @@ from random_walk import RandomWalk
 
 while True:
     #create a random walk and fill
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     #plot
     plt.style.use('seaborn-v0_8')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize = (10, 6), dpi=128)
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values,c=point_numbers, cmap='Blues', edgecolors='none', s=15)
+
+    ax.scatter(rw.x_values, rw.y_values,c=point_numbers, cmap='Blues', edgecolors='none', s=1)
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    #removing the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     running = input("Make another walk? (y/n)")
