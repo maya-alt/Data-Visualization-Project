e994aed6bb0c544f519338dc9b5d82ab155aaf99
maya-alt <mayafisher100@gmail.com>
2025-01-15 10:14:44 -0700

    Update random_walk.py

--- random_walk.py
+++ random_walk.py
@@ -11,23 +11,25 @@ class RandomWalk:
 		self.x_values =[0]
 		self.y_values =[0]
 
-	def fill_walk(self):
-			#calculate the points in a walk
+	def get_step(self):
+		direction = choice([-1, 1])
+		distance = choice([0, 1, 2, 3, 4])
+		return direction * distance
+		
+	
 
-			#keep taking steps until the walk reaches the desired length
+
+	def fill_walk(self):
+
+		#keep taking steps until the walk reaches the desired length
 			while(len(self.x_values) < self.num_points):
 
-				x_direction = choice([1, -1])
-				x_distance = choice([0, 1, 2, 3, 4])
-				x_step = x_direction * x_distance
+				x_step = self.get_step()
+				y_step = self.get_step()
 
-				y_direction = choice([1, -1])
-				y_distance = choice([0, 1, 2, 3, 4])
-				y_step = y_direction * y_distance
-
-				#reject moves that dont go anywhere
 				if x_step == 0 and y_step == 0:
 					continue
+
 
 				x = self.x_values[-1] + x_step
 				y = self.y_values[-1] + y_step
