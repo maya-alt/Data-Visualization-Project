35a6d9917c16ec200dd61b14d8e9040b68ac8c5e
maya-alt <mayafisher100@gmail.com>
2025-01-21 13:18:43 -0700

    Update die.py

--- Rolling-Dice/die.py
+++ Rolling-Dice/die.py
@@ -1,1 +1,8 @@
+from random import randint
 
+class Die:
+    def __init__(self, num_sides=6):
+        self.num_sides = num_sides
+
+    def roll_die(self):
+        return randint(1, self.num_sides)
