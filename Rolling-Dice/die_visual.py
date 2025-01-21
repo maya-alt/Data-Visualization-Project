14799a1bc3aa0c1b890324df0ae1e585beb3bd3c
maya-alt <mayafisher100@gmail.com>
2025-01-21 13:19:26 -0700

    Create die_visual.py

---
+++ Rolling-Dice/die_visual.py
@@ -0,0 +1,27 @@
+from die import Die 
+
+die = Die()
+
+results = []
+
+for roll_num in range(1000):
+    result = die.roll_die()
+    results.append(result)
+
+#analyze the results
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+#visualize results
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of result'}
+my_layout = Layout(title = 'Results of rolling one D6 1000 times', xaxis = x_axis_config,
+yaxis = y_axis_config)
+
+offline.plot({'data':data, 'layout':my_layout}, filename = 'd6.html')
+
